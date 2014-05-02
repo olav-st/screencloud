7ae1e232895db560c0080fabeae4e4bac2971b62
olav-st <olavsortlandthoresen@macmini.local>
2014-05-02 11:38:54 +0200

    fix uninitialized variable in removeDir function

--- screencloud/src/plugin/pluginmanager.cpp
+++ screencloud/src/plugin/pluginmanager.cpp
@@ -310,26 +310,26 @@ void PluginManager::analyzeAndMovePlugin(QString exctractedDirPath)
 
 bool PluginManager::removeDir(const QString &dirName)
 {
-    bool result;
-       QDir dir(dirName);
+    bool result = true;
+    QDir dir(dirName);
 
-       if (dir.exists(dirName)) {
-           Q_FOREACH(QFileInfo info, dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst)) {
-               if (info.isDir()) {
-                   result = removeDir(info.absoluteFilePath());
-               }
-               else {
-                   result = QFile::remove(info.absoluteFilePath());
-               }
-
-               if (!result) {
-                   return result;
-               }
-           }
-           result = dir.rmdir(dirName);
-       }
-       return result;
-
+    if (dir.exists(dirName)) {
+        Q_FOREACH(QFileInfo info, dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst))
+        {
+            if (info.isDir()) {
+                result = removeDir(info.absoluteFilePath());
+            }
+            else
+            {
+                result = QFile::remove(info.absoluteFilePath());
+            }
+            if (!result) {
+                return result;
+            }
+        }
+        result = dir.rmdir(dirName);
+    }
+    return result;
 }
 
 bool PluginManager::copyFolder(QString sourceFolder, QString destFolder)
