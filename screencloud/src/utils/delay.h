d173e8e7542ebbf19e148102f96a4c8d4fe3455e
olav-st <olav.s.th@gmail.com>
2014-06-06 17:07:09 +0200

    add delay to systray check

---
+++ screencloud/src/utils/delay.h
@@ -0,0 +1,15 @@
+#ifndef SLEEP_H
+#define SLEEP_H
+
+#include <QTime>
+
+void delay( int millisecondsToWait )
+{
+    QTime dieTime = QTime::currentTime().addMSecs( millisecondsToWait );
+    while( QTime::currentTime() < dieTime )
+    {
+        QCoreApplication::processEvents( QEventLoop::AllEvents, 100 );
+    }
+}
+
+#endif // SLEEP_H
