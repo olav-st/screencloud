1beae8a7562e349b30616b29a9a133776de8f8a6
TomasKorbar <tomas.korb@seznam.cz>
2018-02-24 21:16:27 +0100

    fix pythonqt_all search on platforms with lib64 directory

--- cmake/modules/FindPythonQt_QtAll.cmake
+++ cmake/modules/FindPythonQt_QtAll.cmake
@@ -3,10 +3,17 @@
 # Sets PYTHONQT_QTALL_FOUND, PYTHONQT_QTALL_INCLUDE_DIR, PYTHONQT_QTALL_LIBRARY, PYTHONQT_QTALL_LIBRARIES
 #
 
+get_property(LIB64 GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS)
+
+if ("${LIB64}" STREQUAL "TRUE")
+    set(LIB_SUFFIX 64)
+else()
+    set(LIB_SUFFIX "")
+endif()
 
 find_path(PYTHONQT_QTALL_INSTALL_DIR NAMES include/PythonQt/PythonQt_QtAll.h include/PythonQt/extensions/PythonQt_QtAll/PythonQt_QtAll.h extensions/PythonQt_QtAll/PythonQt_QtAll.h DOC "Directory where PythonQt_QtAll.h was installed.")
 find_path(PYTHONQT_QTALL_INCLUDE_DIR PythonQt_QtAll.h PATHS "${PYTHONQT_QTALL_INSTALL_DIR}/include/PythonQt" "${PYTHONQT_QTALL_INSTALL_DIR}/include/PythonQt/extensions/PythonQt_QtAll" "${PYTHONQT_QTALL_INSTALL_DIR}/extensions/PythonQt_QtAll" ${CMAKE_INCLUDE_PATH} ${CMAKE_INSTALL_PREFIX}/include ${CMAKE_INSTALL_PREFIX}/include/PythonQt ${CMAKE_INSTALL_PREFIX}/include/PythonQt/extensions/PythonQt_QtAll DOC "Path to the PythonQt_QtAll include directory")
-file(GLOB PYTHONQT_QTALL_LIB_FILE LIST_DIRECTORIES false RELATIVE "${PYTHONQT_QTALL_INSTALL_DIR}/lib" "${PYTHONQT_QTALL_INSTALL_DIR}/lib/*PythonQt_QtAll-Qt[4-9]*.so")
+file(GLOB PYTHONQT_QTALL_LIB_FILE LIST_DIRECTORIES false RELATIVE "${PYTHONQT_QTALL_INSTALL_DIR}/lib${LIB_SUFFIX}" "${PYTHONQT_QTALL_INSTALL_DIR}/lib${LIB_SUFFIX}/*PythonQt_QtAll-Qt[4-9]*.so")
 string(REGEX REPLACE "^lib(.+)\\.so$" "\\1" PYTHONQT_QTALL_LIB "${PYTHONQT_QTALL_LIB_FILE}")
 
 find_library(PYTHONQT_QTALL_LIBRARY NAMES PythonQt_QtAll QtPython_QtAll "${PYTHONQT_QTALL_LIB}" PATHS "${PYTHONQT_QTALL_INSTALL_DIR}/extensions/PythonQt_QtAll" "${PYTHONQT_QTALL_INSTALL_DIR}/lib" DOC "The PythonQt_QtAll library.")
