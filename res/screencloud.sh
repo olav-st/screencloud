e66e57ce8550b5dd7992537bf39103f1cb10579e
olav-st <olav.s.th@gmail.com>
2018-08-03 16:19:52 +0200

    add screencloud.sh as AppRun to set PYTHONPATH

--- res/screencloud.sh
+++ res/screencloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Set PYTHONHOME if running as a snap
 if [ -n "$SNAP" ]; then
+	#Set PYTHONHOME if running as a snap
 	export PYTHONHOME=$SNAP/usr
 	echo "Setting PYTHONHOME=$PYTHONHOME"
 	if ! ls $PYTHONHOME/lib/python* 1> /dev/null 2>&1; then
@@ -8,16 +8,10 @@ if [ -n "$SNAP" ]; then
 	fi
 	export PYTHONPATH=$SNAP/share/screencloud/modules
 	echo "Setting PYTHONPATH=$PYTHONPATH"
-fi
-#Try to figure out the users icon theme
-ICON_THEME=$(gsettings get org.gnome.desktop.interface icon-theme)
-if [ -z "$ICON_THEME" ]; then
-	ICON_THEME=$(gconftool-2 -g /desktop/gnome/interface/icon_theme)
-fi
-#Check for light icon themes
-if [[ "$ICON_THEME" == *light* ]]
-then
-	screencloud --icon-color=black "$@"
 else
-	screencloud "$@"
+	#Do the same, but for AppImage
+	HERE="$(dirname "$(readlink -f "${0}")")"
+	export PYTHONHOME="${HERE}"/usr
+	export PYTHONPATH="${HERE}"/usr/lib/python3.4
+	exec "${HERE}/usr/bin/screencloud" "$@"
 fi
