3e8d28d0ae2413a6492bd727e3d65029e97f629c
olav-st <olav.s.th@gmail.com>
2020-08-04 16:20:16 +0200

    add wayland support by using Gnome/KWin DBus APIs

--- src/screenshooter.h
+++ src/screenshooter.h
@@ -20,39 +20,15 @@
 #include <QDesktopWidget>
 #include <QSettings>
 #include <QScreen>
+#include <QImage>
 
-class ScreenShooter : public QObject
+class ScreenShooter
 {
-    Q_OBJECT
-    Q_PROPERTY(QImage screenshot READ getScreenshot WRITE setScreenshot NOTIFY screenshotTaken)
-    Q_PROPERTY(bool captureWindowBorders READ getCaptureWindowBorders WRITE setCaptureWindowBorders NOTIFY captureWindowBordersChanged)
-    Q_PROPERTY(bool captureMouseCursor READ getCaptureMouseCursor WRITE setCaptureMouseCursor NOTIFY captureMouseCursorChanged)
 
 public:
-    explicit ScreenShooter(QObject *parent = 0);
-
-    void setScreenshot(const QImage &s);
-    QImage getScreenshot();
-
-    void setCaptureWindowBorders(const bool &value);
-    bool getCaptureWindowBorders();
-
-    void setCaptureMouseCursor(const bool &value);
-    bool getCaptureMouseCursor();
-
-    const QImage& captureFullscreen();
-    const QImage& captureSelection(const QRect &area);
-    const QImage& captureWindow(WId windowID = 0);
-
-Q_SIGNALS:
-    void screenshotTaken();
-    void captureWindowBordersChanged();
-    void captureMouseCursorChanged();
-
-private:
-    QImage screenshot;
-    bool captureWindowBorders;
-    bool captureMouseCursor;
+    static const QImage captureFullscreen();
+    static const QImage captureSelection(const QRect &area);
+    static const QImage captureWindow(WId windowID = 0);
 
 };
 
